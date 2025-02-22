16ae503ee513ed3ee9e1aef4dde1d3c640a0aaf3
chemical <gizatulin-vitali@yandex.ru>
2025-02-23 02:10:12 +0500

    14 коммит: Немножко добавил элементов второго окна - кислорода

--- app/src/main/java/com/reftgres/taihelper/ui/oxygen/OxygenMeasurementFragment.kt
+++ app/src/main/java/com/reftgres/taihelper/ui/oxygen/OxygenMeasurementFragment.kt
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.reftgres.taihelper.databinding.FragmentHomeBinding
+import com.reftgres.taihelper.databinding.FragmentOxigenBinding
 
 class OxygenMeasurementFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentOxigenBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -25,13 +25,9 @@ class OxygenMeasurementFragment : Fragment() {
         val oxygenMeasurementViewModel =
             ViewModelProvider(this).get(OxygenMeasurementViewModel::class.java)
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentOxigenBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        oxygenMeasurementViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
     }
 
